668ad5344f64dfef231435c655469a72a8c4b980
vaduga <arbitr38@gmail.com>
2024-01-03 15:59:31 +0800

    fix: provisioning, iconlayer for comments

---
+++ src/deckLayers/IconLayer/icon-layer.ts
@@ -0,0 +1,50 @@
+import {toRGB4Array} from '../../utils';
+import { IconLayer } from '@deck.gl/layers/typed';
+import {toJS} from "mobx";
+import iconAtlas from '/img/location-icon-atlas.png';
+import { colTypes } from '../../store/interfaces';
+
+const ICON_MAPPING = {
+  marker: { x: 0, y: 0, width: 128, height: 128, mask: true },
+};
+
+const MyIconLayer = (props) => {
+  const {
+    data,
+    getSelectedFeIndexes,
+    getSelectedIp,
+    zoom,
+    onHover,
+    highlightColor,
+    iconMapping = ICON_MAPPING,
+  } = props;
+
+    return new IconLayer({
+    highlightColor,
+    onHover,
+    id: colTypes.Icons,
+    iconAtlas,
+    iconMapping,
+    data,
+    selectedFeatureIndexes: getSelectedFeIndexes?.get(colTypes.Icons) ?? [],
+    getPosition: d => d.geometry.coordinates,
+    getIcon: () => 'marker',
+    // @ts-ignore
+    getColor: (d) => {
+      const {iconColor} = d.properties
+      return toRGB4Array(iconColor)
+        },
+    getSize: (d) => 5,//d.properties.locName === getSelectedIp ? zoom * 2 : zoom * 1 ,
+    sizeUnits: 'meters',
+    sizeScale: 0.4,
+    sizeMinPixels: 5,
+    sizeMaxPixels: 45,
+
+
+// Interactive props
+    pickable: true,
+    autoHighlight: true,
+  });
+};
+
+export { MyIconLayer };
